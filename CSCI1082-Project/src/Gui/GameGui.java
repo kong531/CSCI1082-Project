748bfb00ed06c642394bffde9c3c5d95838326d3
Tyler <Tyler@10.0.0.207>
2019-12-08 18:06:23 -0600

    Added a branch for myself

--- CSCI1082-Project/src/Gui/GameGui.java
+++ CSCI1082-Project/src/Gui/GameGui.java
@@ -16,27 +16,26 @@ import javax.swing.JTextArea;
 
 public class GameGui extends JFrame implements ActionListener{
 	
-	private JLabel label1 = new JLabel("Title of Game");
-	private JLabel label2 = new JLabel("Title1");
-	
+	private JLabel label1 = new JLabel("1v1 Sim");
+
 	private Font font = new Font("", Font.BOLD, 50);
 	private Font font1 = new Font("", Font.BOLD, 40);
-	
+
 	private JButton startBtn = new JButton("Start");
-	private JButton btn1 = new JButton("Change Screen");
-	private JButton option1 = new JButton("Option 1");
-	private JButton option2 = new JButton("Option 2");
-	private JButton option3 = new JButton("Option 3");
-	private JButton option4 = new JButton("Option 4");
-	
+	private JButton btn1 = new JButton("Fight!");
+	private JButton option1 = new JButton("Stab");
+	private JButton option2 = new JButton("Punch");
+	private JButton option3 = new JButton("Shoot");
+	private JButton option4 = new JButton("Guard");
+
 	private JTextArea output = new JTextArea();
 
 	private JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 	private JPanel botPanel = new JPanel();
-	private JPanel optionPanel = new JPanel(new GridLayout(4,1));
-	private JPanel newPanel = new JPanel(new GridLayout(2,2));
-	private JPanel textPanel = new JPanel(new GridLayout(1,2));
-	
+	private JPanel optionPanel = new JPanel(new GridLayout(4, 1));
+	private JPanel newPanel = new JPanel(new GridLayout(2, 2));
+	private JPanel textPanel = new JPanel(new GridLayout(1, 2));
+
 	public GameGui(String title) {
 		super(title);
 		setSize(1000, 700);
@@ -50,7 +49,7 @@ public class GameGui extends JFrame implements ActionListener{
 		addPanelsToFrame();
 		additonalSettings();
 		addActionListeners();
-		
+
 	}
 
 	private void createTopPanel() {
@@ -59,28 +58,25 @@ public class GameGui extends JFrame implements ActionListener{
 		label1.setFont(font);
 		label1.setForeground(Color.white);
 		topPanel.setBounds(250, 0, 500, 100);
-		topPanel.add(textPanel);
+		topPanel.add(textPanel, FlowLayout.CENTER);
 		textPanel.setVisible(false);
 	}
-	
+
 	private void createBotPanel() {
 		botPanel.setBackground(Color.black);
 		botPanel.add(optionPanel);
 		botPanel.add(newPanel);
 		newPanel.setVisible(false);
 	}
-	
+
 	private void createOptionPanel() {
 		optionPanel.setBackground(Color.black);
 		optionPanel.setBounds(500, 550, 300, 100);
-		optionPanel.add(startBtn);
-		startBtn.setBackground(Color.black);
-		startBtn.setForeground(Color.white);
 		optionPanel.add(btn1);
 		btn1.setBackground(Color.black);
 		btn1.setForeground(Color.white);
 	}
-	
+
 	private void createNewPanel() {
 		newPanel.setBounds(0, 0, 500, 100);
 		newPanel.add(option1);
@@ -88,51 +84,69 @@ public class GameGui extends JFrame implements ActionListener{
 		newPanel.add(option3);
 		newPanel.add(option4);
 	}
-	
+
 	private void createTextPanel() {
-		textPanel.add(label2);
-		label2.setFont(font1);
-		label2.setBackground(Color.black);
 		textPanel.add(output);
 	}
-	
+
 	private void addPanelsToFrame() {
-		add(topPanel);
-		add(botPanel);
+		add(topPanel, BorderLayout.NORTH);
+		add(botPanel, BorderLayout.CENTER);
 	}
-	
+
 	private void addActionListeners() {
 		startBtn.addActionListener(this);
 		btn1.addActionListener(this);
+		option1.addActionListener(this);
+		option2.addActionListener(this);
+		option3.addActionListener(this);
+		option4.addActionListener(this);
 	}
-	
+
 	private void additonalSettings() {
 		output.setEditable(false);
 		output.setBackground(Color.white);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String callingBtn = e.getActionCommand();
-		
-		if (callingBtn.equals("Start")) {
-			optionPanel.setVisible(false);
-			newPanel.setVisible(true);
-			label1.setVisible(false);			
-			textPanel.setVisible(true);
-		}
-		
-		else if (callingBtn.equals("Change Screen")) {
+
+		if (callingBtn.equals("Fight!")) {
+			topPanel.setBackground(Color.white);
 			optionPanel.setVisible(false);
 			newPanel.setVisible(true);
 			label1.setVisible(false);
 			textPanel.setVisible(true);
-			setLayout(new GridLayout(1,2));
-			topPanel.setBackground(Color.blue);
+			setLayout(new GridLayout(2, 2));
+		} else if (callingBtn.equals("Stab")) {
+			optionPanel.setVisible(false);
+			newPanel.setVisible(true);
+			label1.setVisible(false);
+			textPanel.setVisible(true);
+			output.append("You did 8 damage \n");
+		} else if (callingBtn.equals("Punch")) {
+			optionPanel.setVisible(false);
+			newPanel.setVisible(true);
+			label1.setVisible(false);
+			textPanel.setVisible(true);
+			output.append("You did 5 damage \n");
+		} else if (callingBtn.equals("Shoot")) {
+			optionPanel.setVisible(false);
+			newPanel.setVisible(true);
+			label1.setVisible(false);
+			textPanel.setVisible(true);
+			output.append("You did 3 damage \n");
+		} else if (callingBtn.equals("Guard")) {
+			optionPanel.setVisible(false);
+			newPanel.setVisible(true);
+			label1.setVisible(false);
+			textPanel.setVisible(true);
+			output.append("You took no damage \n");
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		GameGui game = new GameGui("GAME");
 		game.setVisible(true);
